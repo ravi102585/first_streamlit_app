149aaa15b2f7d091d2ba200cc4cf376bcc5a9538
ravikumardudala <ravi102585@gmail.com>
2022-07-28 12:25:58 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My MoM\'s New Healthy Diner')
-
+#streamlit.title('My MoM\'s New Healthy Diner')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header(' Breakfast Favototes')
 streamlit.text('🍳 omega 3 & Blueberry Oatmeal')
 streamlit.text('🌽kale, Spinach & Rocket Smoothie')
@@ -8,3 +9,4 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌 🍍Build Your Own Fruit Smoothie🥝🍇')
+streamlit.dataframe(my_fruit_list)
