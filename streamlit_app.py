34dc165cfd3a51dd91bfe9c36a105e5ae55155b6
ravikumardudala <ravi102585@gmail.com>
2022-07-28 12:06:10 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-
+import emoji
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('\N{flexd biceps}Breakfast Menu\N{flexd biceps}')
+streamlit.header(emoji.emojize('python is : thumbs_up:')+' Breakfast Menu')
 streamlit.text('omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
