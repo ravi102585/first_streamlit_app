57923fa86727172f62f41980921f7fcfa16f7136
ravikumardudala <ravi102585@gmail.com>
2022-07-28 11:43:10 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit
 
+streamlit.title('My Parents New Healthy Diner')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    # Press ⌘F8 to toggle the breakpoint.
-    streamlit.title('My Parents New Healthy Diner')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
