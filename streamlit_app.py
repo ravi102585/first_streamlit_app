124e1eff4a987f9615e8b6474c1cbeb2ff4b2482
ravikumardudala <ravi102585@gmail.com>
2022-07-29 13:11:35 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,17 +21,23 @@ fruits_selected = streamlit.multiselect('Pick some fruits : ', list(my_fruit_lis
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+     if not fruit_choice:
+         streamlit.error("please select a fruit to get information")
+     else:
+         #import requests
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+         #streamlit.text(fruityvice_response.json())
+         # write your own comment -what does the next line do?
+         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+         # write your own comment - what does this do?
+         streamlit.dataframe(fruityvice_normalized)
+         #streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+    streamlit.error()
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do?
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 
