6ecd60f586c98d04b0ff511765379d7ac159a22e
ravikumardudala <ravi102585@gmail.com>
2022-07-29 13:20:23 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,24 @@ fruits_selected = streamlit.multiselect('Pick some fruits : ', list(my_fruit_lis
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruitvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # streamlit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do?
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
      if not fruit_choice:
          streamlit.error("please select a fruit to get information")
      else:
          #import requests
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-         #streamlit.text(fruityvice_response.json())
-         # write your own comment -what does the next line do?
-         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
          # write your own comment - what does this do?
-         streamlit.dataframe(fruityvice_normalized)
+         back_from_fundtion=get_fruitvice_data(fruit_choice)
+         streamlit.dataframe(back_from_fundtion)
          #streamlit.write('The user entered ', fruit_choice)
 except URLError as e:
     streamlit.error()
