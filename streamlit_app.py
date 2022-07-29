4436c3670f060042cc869f37f054451c63173020
ravikumardudala <ravi102585@gmail.com>
2022-07-29 13:46:32 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_row =get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_row)
 
 def insert_row_snowflake(new_fruit):
@@ -68,4 +69,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like load?')
 if streamlit.button('Add Fruit to the list '):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_fundtion = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_fundtion)
