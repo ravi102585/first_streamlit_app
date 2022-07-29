dcb7a3d3b3ccd64317738d91d9c493299d7112e8
ravikumardudala <ravi102585@gmail.com>
2022-07-29 09:00:39 -0500

    added first file to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-#streamlit.title('My MoM\'s New Healthy Diner')
+streamlit.title('My MoM\'s New Healthy Diner')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
